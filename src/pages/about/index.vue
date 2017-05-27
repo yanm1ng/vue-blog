e8a1cb6844559417c8fd26d0970dfbd8d174e106
yanm1ng <yummy1121@foxmail.com>
2017-05-27 18:46:25 +0800

    added markdown filter

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,6 +1,10 @@
 <template>
   <main class="app-body">
-    关于我
+    <h3 class="about-me">关于我</h3>
+    <ul class="intro-list">
+      <li><span class="not">985、211大学</span> 计算机大三学生</li>
+      <li></li>
+    </ul>
   </main>
 </template>
 
@@ -11,6 +15,24 @@ export default {
 </script>
 
 <style scoped>
-
+.about-me {
+  position: relative;
+}
+.about-me:before {
+  content: "#";
+  color: #42b983;
+  position: absolute;
+  left: -.7em;
+  top: -2px;
+  font-size: 1.2em;
+  font-weight: 700;
+}
+.intro-list {
+  padding: 1em;
+  font-size: 15px;
+}
+.not {
+  text-decoration: line-through;
+}
 </style>
 
